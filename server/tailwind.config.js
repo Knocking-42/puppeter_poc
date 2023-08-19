e1f7463f7f930bf9da4112fa34e7180602cbb537
joonhyeok han <virq@naver.com>
2023-08-20 01:44:37 +0900

    build: :construction_worker: Modify css build configuration

--- server/tailwind.config.js
+++ server/tailwind.config.js
@@ -1,6 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./views/**/*.{hbs,js}'],
+  content: [
+    './views/**/*.{hbs,js}',
+    './src/**/*.{html,js}',
+    './pages/**/*.{html,js}',
+    './components/**/*.{html,js}',
+  ],
   theme: {
     extend: {},
   },
